f1bebeb4fd9dab2be0bf7e7a1c296a2a33996daa
Administrator <admin@example.com>
2022-04-20 17:01:50 +0800

    第二题

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"love/gobase"
 )
 
 func main() {
-	fmt.Println("lol")
-	
+
+	//letcode.TwoSum()
+	//letcode.AddTwoNumbers()
+	list1 := []int{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+	l11 := gobase.AddListNode(list1)
+
+	list2 := []int{0}
+	l22 := gobase.AddListNode(list2)
+	Node := gobase.ListNodeMultiPlusSecondVersion(l11, l22)
+	fmt.Println(gobase.ListNodeToSrting(&Node))
 }
